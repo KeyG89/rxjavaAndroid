3441b0062ebaac14ece968c17a2eabb8db06215b
KeyG89 <Krzysztof.Goscinski@gmail.com>
2019-02-09 00:02:46 +0100

    3. Network: Showing our results

--- app/src/main/java/com/jonbott/learningrxjava/Activities/NetworkExample/NetworkExampleActivity.kt
+++ app/src/main/java/com/jonbott/learningrxjava/Activities/NetworkExample/NetworkExampleActivity.kt
@@ -5,18 +5,28 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import com.jonbott.learningrxjava.Activities.NetworkExample.Recycler.MessageViewAdapter
+import com.jonbott.learningrxjava.Common.disposedBy
 import com.jonbott.learningrxjava.R
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_network_example.*
 
 class NetworkExampleActivity : AppCompatActivity() {
 
     private val presenter = NetworkExamplePresenter()
     lateinit var adapter: MessageViewAdapter
+    private var bag = CompositeDisposable()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_network_example)
+
+        presenter.messages
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe{messages ->
+                    adapter.messages.accept(messages)
+                }.disposedBy(bag)
 
         attachUI()
     }
@@ -39,7 +49,12 @@ class NetworkExampleActivity : AppCompatActivity() {
 
     private fun rowTapped(position: Int) {
         println("🍄")
-        println(adapter.messages[position])
+        println(adapter.messages.value[position])
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        bag.clear()
     }
 }
 
