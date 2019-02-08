20f814ce58b0816a6e42468f2d5a73029a4d6ad1
KeyG89 <Krzysztof.Goscinski@gmail.com>
2019-02-08 19:10:30 +0100

    2. Rx single creation - (retrofit onFailure&onResponse)

--- app/src/main/java/com/jonbott/learningrxjava/Activities/BasicExampleActivity.kt
+++ app/src/main/java/com/jonbott/learningrxjava/Activities/BasicExampleActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.jonbott.learningrxjava.ModelLayer.Entities.Posting
 import com.jonbott.learningrxjava.R
+import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import java.io.IOException
 
 
 class BasicExampleActivity : AppCompatActivity() {
@@ -35,7 +39,45 @@ class BasicExampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basic_example)
+
+        realSingleExample()
     }
 
-    //endregion
+    // region Rx Code
+    private fun realSingleExample() {
+
+    }
+
+    private fun loadPostAsSingle(): Single<Posting> {
+        return Single.create { observer ->
+            // Simulate network delay
+            Thread.sleep(2000)
+
+            val postingId = 5
+            service.getPosts(postingId.toString()).enqueue(object : Callback<Posting> {
+
+                override fun onFailure(call: Call<Posting>, t: Throwable) {
+                    val e = t ?: IOException("An Unknown network error occurred")
+                    observer.onError(e)
+
+                }
+
+                override fun onResponse(call: Call<Posting>, response: Response<Posting>) {
+                    val posting = response?.body()
+
+                    if (posting != null) {
+                        observer.onSuccess(posting)
+                    } else {
+                        val e = IOException("An Unknown network error occurred")
+                        observer.onError(e)
+                    }
+
+
+                }
+
+
+            })
+        }
+    }
 }
+
