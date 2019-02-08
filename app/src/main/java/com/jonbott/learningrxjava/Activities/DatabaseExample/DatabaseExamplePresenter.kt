d7a14be8b42c6ead33bc9b10d38b6ba2b7ed63ee
KeyG89 <Krzysztof.Goscinski@gmail.com>
2019-02-08 20:02:41 +0100

    3. Database: From model layer to UI

--- app/src/main/java/com/jonbott/learningrxjava/Activities/DatabaseExample/DatabaseExamplePresenter.kt
+++ app/src/main/java/com/jonbott/learningrxjava/Activities/DatabaseExample/DatabaseExamplePresenter.kt
@@ -1,7 +1,24 @@
 package com.jonbott.learningrxjava.Activities.DatabaseExample
 
+import com.jakewharton.rxrelay2.BehaviorRelay
 import com.jonbott.learningrxjava.ModelLayer.ModelLayer
+import com.jonbott.learningrxjava.ModelLayer.PersistenceLayer.PhotoDescription
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class DatabaseExamplePresenter {
     val modelLayer = ModelLayer.shared //normally injected
+
+    val photoDescriptions: BehaviorRelay<List<PhotoDescription>>
+        get() = modelLayer.photoDescriptions // bubbling up for the lower layer
+
+    init {
+        GlobalScope.launch {
+            delay(3000) // 3 seconds
+            modelLayer.loadAllPhotoDescriptions()
+
+        }
+    }
+
 }
