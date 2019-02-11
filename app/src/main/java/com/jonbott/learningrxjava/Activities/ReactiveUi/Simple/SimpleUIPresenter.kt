c1b66b64c922f577a09f8d18dfef16e5b0e181b0
KeyG89 <Krzysztof.Goscinski@gmail.com>
2019-02-11 23:18:42 +0100

    5. Binding to the UI

--- app/src/main/java/com/jonbott/learningrxjava/Activities/ReactiveUi/Simple/SimpleUIPresenter.kt
+++ app/src/main/java/com/jonbott/learningrxjava/Activities/ReactiveUi/Simple/SimpleUIPresenter.kt
@@ -1,26 +1,66 @@
 package com.jonbott.learningrxjava.Activities.ReactiveUi.Simple
 
+import com.jakewharton.rxrelay2.BehaviorRelay
 import com.jonbott.learningrxjava.ModelLayer.Entities.Friend
+import io.reactivex.subjects.BehaviorSubject
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SimpleUIPresenter {
-    var friends = listOf(Friend("Debi", "Darlington"),
-                         Friend("Arlie", "Abalos"),
-                         Friend("Jessica", "Jetton"),
-                         Friend("Tonia", "Threlkeld"),
-                         Friend("Donte", "Derosa"),
-                         Friend("Nohemi", "Notter"),
-                         Friend("Rod", "Rye"),
-                         Friend("Simonne", "Sala"),
-                         Friend("Kathaleen", "Kyles"),
-                         Friend("Loan", "Lawrie"),
-                         Friend("Elden", "Ellen"),
-                         Friend("Felecia", "Fortin"),
-                         Friend("Fiona", "Fiorini"),
-                         Friend("Joette", "July"),
-                         Friend("Beverley", "Bob"),
-                         Friend("Artie", "Aquino"),
-                         Friend("Yan", "Ybarbo"),
-                         Friend("Armando", "Araiza"),
-                         Friend("Dolly", "Delapaz"),
-                         Friend("Juliane", "Jobin"))
+
+    val friends = BehaviorRelay.createDefault(listOf<Friend>())
+    val title = BehaviorSubject.createDefault("Default Title")
+
+    init {
+        loadFriends()
+    }
+
+    private fun loadFriends() {
+        title.onNext("LoadFriends")
+
+        GlobalScope.launch {
+            delay(300)
+            title.onNext("Friends Loaded")
+
+            var newFriends = listOf(Friend("Debi", "Darlington"),
+                    Friend("Arlie", "Abalos"),
+                    Friend("Jessica", "Jetton"),
+                    Friend("Tonia", "Threlkeld"),
+                    Friend("Donte", "Derosa"),
+                    Friend("Nohemi", "Notter"),
+                    Friend("Rod", "Rye"),
+                    Friend("Simonne", "Sala"),
+                    Friend("Kathaleen", "Kyles"),
+                    Friend("Loan", "Lawrie"),
+                    Friend("Elden", "Ellen"),
+                    Friend("Felecia", "Fortin"),
+                    Friend("Fiona", "Fiorini"),
+                    Friend("Joette", "July"),
+                    Friend("Beverley", "Bob"),
+                    Friend("Artie", "Aquino"),
+                    Friend("Yan", "Ybarbo"),
+                    Friend("Armando", "Araiza"),
+                    Friend("Dolly", "Delapaz"),
+                    Friend("Juliane", "Jobin"))
+
+            launch {
+                friends.accept(newFriends)
+            }
+        }
+
+        GlobalScope.launch {
+            delay(6000) // 6 seconds
+            title.onError(Exception("Faked Error"))
+        }
+
+        GlobalScope.launch {
+            delay(7000) // 7 seconds
+            println("🎶🎶 pushing new value")
+            title.onNext("new Value")
+        }
+    }
+
+
 }
+
