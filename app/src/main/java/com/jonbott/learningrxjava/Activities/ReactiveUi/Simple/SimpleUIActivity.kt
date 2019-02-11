1df3d5b6a36ebb0669ee0a2deecb9af9a5b7d40f
KeyG89 <Krzysztof.Goscinski@gmail.com>
2019-02-11 23:39:05 +0100

    5. Sample binding - share() - code triggered once for every sub & onErrorReturn handles the error resulting with Default Value

--- app/src/main/java/com/jonbott/learningrxjava/Activities/ReactiveUi/Simple/SimpleUIActivity.kt
+++ app/src/main/java/com/jonbott/learningrxjava/Activities/ReactiveUi/Simple/SimpleUIActivity.kt
@@ -3,11 +3,16 @@ package com.jonbott.learningrxjava.Activities.ReactiveUi.Simple
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
+import com.jonbott.learningrxjava.Common.disposedBy
 import com.jonbott.learningrxjava.R
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_simple_ui.*
 
 class SimpleUIActivity : AppCompatActivity() {
 
+
+    private var bag = CompositeDisposable()
     private val presenter = SimpleUIPresenter()
     private lateinit var adapter: ArrayAdapter<String>
 
@@ -19,15 +24,34 @@ class SimpleUIActivity : AppCompatActivity() {
     }
 
     private fun rxExamples() {
+
+        simpleRx()
         rxSimpleListBind()
+    }
+
+    private fun simpleRx() {
+//        presenter.title.observeOn(AndroidSchedulers.mainThread())
+//                .subscribe{ simpleUITitleTextView.text = it}
+//                .disposedBy(bag)
+
+        presenter.title.observeOn(AndroidSchedulers.mainThread())
+                .onErrorReturn { "Default Value" }
+                .share() // code triggered once for every subscriber without duplicating an action each and every time
+                .subscribe{ simpleUITitleTextView.text = it}
+                .disposedBy(bag)
     }
 
 
     private fun rxSimpleListBind() {
-        val listItems = presenter.friends.map { it.toString() }
+        val listItems = presenter.friends.value.map { it.toString() }
 
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
 
         simpleUIListView.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        bag.clear()
     }
 }
