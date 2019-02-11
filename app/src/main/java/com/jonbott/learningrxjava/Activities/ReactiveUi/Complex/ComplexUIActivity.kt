65abe8f992408d8938d65390111dbdadd257c328
KeyG89 <Krzysztof.Goscinski@gmail.com>
2019-02-12 00:26:37 +0100

    5. RecyclerView: Binding setup - must recompile project after using 'layout' in xml file

--- app/src/main/java/com/jonbott/learningrxjava/Activities/ReactiveUi/Complex/ComplexUIActivity.kt
+++ app/src/main/java/com/jonbott/learningrxjava/Activities/ReactiveUi/Complex/ComplexUIActivity.kt
@@ -3,6 +3,8 @@ package com.jonbott.learningrxjava.Activities.ReactiveUi.Complex
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.jonbott.learningrxjava.R
+import com.jonbott.learningrxjava.databinding.ActivityComplexUiBinding
+import io.reactivex.disposables.CompositeDisposable
 
 
 //https://github.com/ahmedrizwan/RxRecyclerAdapter
@@ -15,9 +17,23 @@ private enum class CellType {
 class ReactiveUIActivity : AppCompatActivity() {
 
     private val dataSet = (0..100).toList().map { it.toString() }
+    private var bag = CompositeDisposable()
+
+    lateinit var boundActivity: ActivityComplexUiBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_complex_ui)
+        commonInit()
+        showSimpleBindingExample()
+    }
+
+    private fun showSimpleBindingExample() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun commonInit() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
